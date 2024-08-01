c8bf0489adb501644ec8667b222df5e5b6172d2e
sandiarbaa <sandiarbaa@gmail.com>
2024-08-02 05:36:45 +0700

    passing data dari route ke template view

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/random", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  // res.render("random", { random: num }); // cara 1 passing data ke template | menggunakan alias
+  res.render("random", { num }); // cara 2 passing data ke template | langsung destructuring karena dia objek
+});
+
 app.listen(8080, () => {
   console.log("Listening on port http://localhost:8080");
 });
