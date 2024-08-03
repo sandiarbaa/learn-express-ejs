d700974f62eb40005402572ab8f6185d8bb4d257
sandiarbaa <sandiarbaa@gmail.com>
2024-08-03 07:27:37 +0700

    membuat static folder untuk assets web

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 const tagsData = require("./data.json");
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Routes
 app.get("/", (req, res) => {
